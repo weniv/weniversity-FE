219538f5a567f57160e69e08c25bdf2ab0ef682d
gyeongbaek <gyeongbaek110@gmail.com>
2024-11-04 17:29:58 +0900

    :tada: Feat: 프로젝트 기본 세팅

---
+++ src/components/header/LogoHeader.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LogoHeader() {
+  return <div>LogoHeader</div>;
+}
---
+++ src/components/header/NavHeader.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NavHeader() {
+  return <div>NavHeader</div>;
+}
