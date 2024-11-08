0dd8919f08f5dba67eeaf2ad729be7ac10a49646
gyeongbaek <gyeongbaek110@gmail.com>
2024-11-08 15:05:07 +0900

    :bug: Fix: 순회하는 컴포넌트 key props 추가

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,7 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   assetPrefix: isProd ? "https://www.weniversity.co.kr" : undefined,
-  experimental: {
-    incremtnalCaching: true,
-  },
+  images: { unoptimized: true },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
--- src/app/courses/[id]/page.jsx
+++ src/app/courses/[id]/page.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function course() {
-  return <div>course</div>;
+export async function generateStaticParams() {
+  const courses = [{ id: "1" }, { id: "2" }];
+
+  return courses.map((course) => ({
+    id: course.id,
+  }));
+}
+
+export default function course({ params }) {
+  return <div>Course {params.id}</div>;
 }
