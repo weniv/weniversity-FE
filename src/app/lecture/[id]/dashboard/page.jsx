8ece5ad7d6bf6c1b8cff41daf1acb73ba8ae6f0e
gyeongbaek <gyeongbaek110@gmail.com>
2024-11-08 15:07:29 +0900

    :memo: Edit: generateStaticParams 코드 추가

--- src/app/lecture/[id]/dashboard/page.jsx
+++ src/app/lecture/[id]/dashboard/page.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function lecture() {
+export async function generateStaticParams() {
+  const courses = [{ id: "1" }, { id: "2" }];
+
+  return courses.map((course) => ({
+    id: course.id,
+  }));
+}
+export default function lecture({ params }) {
   return <section>강의실</section>;
 }
