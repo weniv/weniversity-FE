40cd2f4dbd248e6756ec038f8d43e5c5434a3d2c
gyeongbaek <gyeongbaek110@gmail.com>
2024-11-08 14:37:40 +0900

    :package: Chore: 배포를 위한 next 속성 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === "production";
 
+const nextConfig = {
+  output: "export",
+  assetPrefix: isProd ? "https://www.weniversity.co.kr" : undefined,
+};
+
+module.exports = nextConfig;
 export default nextConfig;
