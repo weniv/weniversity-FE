31d645e00753b9ba7f3c2c93458f36733bea2a3d
gyeongbaek <gyeongbaek110@gmail.com>
2024-11-08 14:49:49 +0900

    :bug:FIx: 배포 오류 수정

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   assetPrefix: isProd ? "https://www.weniversity.co.kr" : undefined,
+  experimental: {
+    incremtnalCaching: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
